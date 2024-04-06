818fde539ebe410c9d313a28a33c57ce5058e03f
Muhammad Cikal Merdeka <mcikalmerdeka@gmail.com>
2024-04-06 09:10:56 +0700

    Finished query for task 3

--- Task 3/Task 3 - Annual Product Category Quality Analysis.sql
+++ Task 3/Task 3 - Annual Product Category Quality Analysis.sql
@@ -1,177 +1,118 @@
--- TASK 2 : ANNUAL CUSTOMER ACTIVITY GROWTH ANALYSIS
+-- TASK 3 : ANNUAL PRODUCT CATEGORY QUALITY ANALYSIS
 
--- ========== A. Display the average monthly active user count for each year ==========
+DROP TABLE IF EXISTS total_revenue_yearly; 
+DROP TABLE IF EXISTS total_cancel_orders_yearly;
+DROP TABLE IF EXISTS top_product_category_revenue_yearly; 
+DROP TABLE IF EXISTS top_product_category_canceled_yearly; 
 
--- Hint: Pay attention to the consistency of the date format.
+-- ========== A. Create table that contains total revenue for each year ==========
 
+-- Hint: Revenue consists of the price of goods as well as shipping costs.
+-- Also, ensure to filter out the appropriate order statuses to calculate revenue.
+
+CREATE TABLE IF NOT EXISTS total_revenue_yearly AS
 SELECT
-	year,
-	FLOOR(AVG(customer_total)) AS avg_monthly_active_user
-FROM (
-    SELECT
-        EXTRACT(year FROM o.order_purchase_timestamp) AS year,
-        EXTRACT(month FROM o.order_purchase_timestamp) AS month,
-        COUNT(DISTINCT c.customer_unique_id) AS customer_total
-    FROM orders o
-    JOIN customers c
-	ON o.customer_id = c.customer_id
-    GROUP BY 1, 2
-) AS monthly
-GROUP BY 1
+	EXTRACT(year FROM o.order_purchase_timestamp) AS year,
+    ROUND(SUM(oi.price + oi.freight_value)::numeric,2) AS total_revenue
+FROM orders o
+JOIN order_items oi
+ON o.order_id = oi.order_id
+WHERE o.order_status = 'delivered'
+GROUP by 1
 ORDER BY 1;
 
--- ========== B. Display the number of new customers for each year ==========
+-- ========== B. Create table that contains total cancel order for each year ==========
 
--- Hint: New customers are those who place their first order.
+-- Hint: Pay attention to filtering out the appropriate order status to calculate the number of canceled orders.
 
+CREATE TABLE IF NOT EXISTS total_cancel_orders_yearly AS
 SELECT
-	EXTRACT(year FROM first_order_date) AS year,
-	COUNT(customer_unique_id) AS total_new_customer
-FROM(
-	SELECT
-		c.customer_unique_id,
-		MIN(o.order_purchase_timestamp) AS first_order_date
-	FROM orders o
-	JOIN customers c
-	ON o.customer_id = c.customer_id
-	GROUP BY 1
-) AS yearly_first_order
-GROUP BY 1
+	EXTRACT(year FROM order_purchase_timestamp) AS year,
+    COUNT(order_id) AS total_cancel_order
+FROM orders
+WHERE order_status = 'canceled'
+GROUP by 1
 ORDER BY 1;
 
--- ========== C. Display the number of customers who made more than one purchase (repeat orders) for each year ==========
+-- ========== C. Create table containing the names of product categories that provide the highest total revenue for each year ==========
 
--- Hint: Repeat customers are those who place more than 1 order.
+-- Hint: Hint: Pay attention to the use of window functions and also the applied filtering
 
-SELECT
-	year,
-	COUNT(DISTINCT customer_unique_id) AS total_repeat_customer
-FROM(
+CREATE TABLE top_product_category_revenue_yearly AS 
+WITH revenue_category_orders AS (
 	SELECT
-		EXTRACT(year FROM o.order_purchase_timestamp) AS year,	
-		c.customer_unique_id,
-		COUNT(c.customer_unique_id) AS total_customer,
-		COUNT(o.order_id) AS total_order
+		EXTRACT(year FROM order_purchase_timestamp) AS year,
+		p.product_category_name,
+		ROUND(SUM(oi.price + oi.freight_value)::numeric,2) AS total_revenue,
+		RANK() OVER(
+			PARTITION BY EXTRACT(year FROM order_purchase_timestamp)
+			ORDER BY SUM(oi.price + oi.freight_value) DESC
+		) AS ranking
 	FROM orders o
-	JOIN customers c
-	ON o.customer_id = c.customer_id
+	JOIN order_items oi
+	ON o.order_id = oi.order_id
+	JOIN products p
+	ON oi.product_id = p.product_id
+	WHERE o.order_status = 'delivered'
 	GROUP BY 1,2
-	HAVING COUNT(o.order_id) > 1
-) AS yearly_repeat_order
-GROUP BY 1
-ORDER BY 1;
-
--- ========== D. Display the average number of orders made by customers for each year ==========
-
--- Hint: Calculate the order frequency (how many times they ordered) for each customer first.
+)
 
 SELECT
 	year,
-	ROUND(AVG(order_freq), 2) AS average_order
-FROM(
+	product_category_name,
+	total_revenue
+FROM revenue_category_orders
+WHERE ranking = 1;
+
+-- ========== D. Create table containing the names of product categories that have the highest number of canceled orders for each year.= ==========
+
+-- Hint: Hint: Pay attention to the use of window functions and the applied filtering
+
+CREATE TABLE top_product_category_canceled_yearly AS 
+WITH product_category_canceled_orders AS (
 	SELECT
-		EXTRACT(year FROM o.order_purchase_timestamp) AS year,	
-		c.customer_unique_id,
-		COUNT(c.customer_unique_id) AS total_customer,
-		COUNT(o.order_id) AS order_freq
+		EXTRACT(year FROM order_purchase_timestamp) AS year,
+		p.product_category_name,
+    	COUNT(o.order_id) AS total_cancel_order,
+		RANK() OVER(
+			PARTITION BY EXTRACT(year FROM order_purchase_timestamp)
+			ORDER BY COUNT(o.order_id) DESC
+		) AS ranking
 	FROM orders o
-	JOIN customers c
-	ON o.customer_id = c.customer_id
+	JOIN order_items oi
+	ON o.order_id = oi.order_id
+	JOIN products p
+	ON oi.product_id = p.product_id
+	WHERE o.order_status = 'canceled'
 	GROUP BY 1,2
-) AS yearly_avg_order
-GROUP BY 1
-ORDER BY 1;
-
--- ========== E. Combine the three successfully displayed metrics into a single table view ==========
-
-WITH cte_mau AS (
-	SELECT
-		year,
-		FLOOR(AVG(customer_total)) AS avg_monthly_active_user
-	FROM (
-		SELECT
-			EXTRACT(year FROM o.order_purchase_timestamp) AS year,
-			EXTRACT(month FROM o.order_purchase_timestamp) AS month,
-			COUNT(DISTINCT c.customer_unique_id) AS customer_total
-		FROM orders o
-		JOIN customers c
-		ON o.customer_id = c.customer_id
-		GROUP BY 1, 2
-	) AS monthly
-	GROUP BY 1
-	ORDER BY 1
-),
-
-cte_new_cust AS (
-	SELECT
-		EXTRACT(year FROM first_order_date) AS year,
-		COUNT(customer_unique_id) AS total_new_customer
-	FROM(
-		SELECT
-			c.customer_unique_id,
-			MIN(o.order_purchase_timestamp) AS first_order_date
-		FROM orders o
-		JOIN customers c
-		ON o.customer_id = c.customer_id
-		GROUP BY 1
-	) AS yearly_first_order
-	GROUP BY 1
-	ORDER BY 1
-),
-
-cte_repeat_order AS (
-	SELECT
-		year,
-		COUNT(DISTINCT customer_unique_id) AS total_repeat_customer
-	FROM(
-		SELECT
-			EXTRACT(year FROM o.order_purchase_timestamp) AS year,	
-			c.customer_unique_id,
-			COUNT(c.customer_unique_id) AS total_customer,
-			COUNT(o.order_id) AS total_order
-		FROM orders o
-		JOIN customers c
-		ON o.customer_id = c.customer_id
-		GROUP BY 1,2
-		HAVING COUNT(o.order_id) > 1
-	) AS yearly_repeat_order
-	GROUP BY 1
-	ORDER BY 1
-),
-
-cte_avg_order AS (
-	SELECT
-		year,
-		ROUND(AVG(order_freq), 2) AS average_order
-	FROM(
-		SELECT
-			EXTRACT(year FROM o.order_purchase_timestamp) AS year,	
-			c.customer_unique_id,
-			COUNT(c.customer_unique_id) AS total_customer,
-			COUNT(o.order_id) AS order_freq
-		FROM orders o
-		JOIN customers c
-		ON o.customer_id = c.customer_id
-		GROUP BY 1,2
-	) AS yearly_avg_order
-	GROUP BY 1
 	ORDER BY 1
 )
 
 SELECT
-	mau.year AS year,
-	mau.avg_monthly_active_user,
-	nc.total_new_customer,
-	ro.total_repeat_customer,
-	ao.average_order
+	year,
+	product_category_name,
+	total_cancel_order
+FROM product_category_canceled_orders
+WHERE ranking = 1;
 
-FROM cte_mau mau
-JOIN cte_new_cust nc
-ON mau.year = nc.year
-JOIN cte_repeat_order ro
-ON nc.year = ro.year
-JOIN cte_avg_order ao
-ON ro.year = ao.year
 
-GROUP BY 1,2,3,4,5
-ORDER BY 1;
+-- ========== E. Combining the obtained information into a single table view ==========
+
+-- Hint: Pay attention to the join technique applied and the selected columns
+
+SELECT
+	tpr.year, 
+	tpr.product_category_name AS top_product_category_revenue_name, 
+	tpr.total_revenue AS top_product_category_revenue, 
+	try.total_revenue AS total_revenue_yearly,
+	tpc.product_category_name AS top_product_category_cancel_name,
+	tpc.total_cancel_order AS top_product_category_cancel,
+	tcy.total_cancel_order AS total_cancel_orders_yearly
+	
+FROM top_product_category_revenue_yearly tpr
+JOIN total_revenue_yearly try 
+ON tpr.year = try.year
+JOIN top_product_category_canceled_yearly tpc
+ON try.year = tpc.year
+JOIN total_cancel_orders_yearly tcy
+ON tpc.year = tcy.year;
